21607fd44fbabb9403875872e66f7a3c6ee92330
Jose De Arimateia Lima Da Silva <TAE11946@somoseducacao.com.br>
2020-05-21 11:10:58 -0300

    criaçao do projeto de cadastro de produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 
+function App (props) {
 
-function ComponentFuncional(){
-  return(    
-    <h1>Teste</h1>
-  )
-}
+  //Função em aero funciont
+  const modificarNome = (event) => {
+    console.log(event.target.value)
+  }
 
-class App extends React.Component{
-  render(){
-    return(    
-      <h1> Hello From App</h1>
+  const criaComboBox = () => {
+    const opcoes = ["São Paulo","Rio de Janeiro","Espirito Santo","Minas Gerais"]
+    const comboBoxOptcoes = opcoes.map( opcao => <option>{opcao}</option>)
+
+    return(
+      <select>
+        {comboBoxOptcoes}
+      </select>
     )
   }
+
+  const MeuComboBox = () => criaComboBox()
+
+  return( 
+    //usando react.fragment como <>
+    <>
+      <input type="text" value={props.nome} onChange={modificarNome}  className="texto-centralizado" />
+  <h1> Hello {props.nome} - {props.idade} - {props.sexo}</h1>
+      <MeuComboBox />
+    </> 
+  )
+  
 }
 
 export default App;
--- iniciando-com-react/src/index.js
+++ iniciando-com-react/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './index.css'
+
 ReactDOM.render(
-    <App />,
+    <App nome="José Silva" idade="33" sexo="M"/>,
   document.getElementById('root')
 );
